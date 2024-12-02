e45d088e08145ac8e77b232477c9dd3a7915363c
gtewary07 <gtewary@asu.edu>
2024-12-02 03:26:47 -0700

    Create NeoServer.js

---
+++ NeoServer.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('colorChange', (color) => {
+    console.log('Color changed to:', color);
+    io.emit('updateColor', color);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
